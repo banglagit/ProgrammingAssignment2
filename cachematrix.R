9658be0686e458f7610415bd48475eb18fcd91d7
banglagit <istiakahmed2033@gmail.com>
2020-09-30 22:44:09 +0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,9 @@
 ## it accelerates the inverse matrix calculation
+## sometimes it is cumbersome on the part of cpu to actually carry out computing
+## out computing task in case of big data. so in those cases,
+## it is useful to take advantage of cache memory which is very fast 
+## component to work with. This function no matter how cryptic it looks 
+## does exactly the same.
 
 makeCacheMatrix <- function(x = matrix()) {
   j <- NULL
@@ -16,7 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-##it accelerates the inverse matrix calculation
+## the workflow is like: it first checks if there is a already calculated 
+## inv_function, If there is one thw progran uses that but if there is not one
+## the program rerruns the function and computes it
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
